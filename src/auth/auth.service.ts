8dfcf17cb2dd22561ef11bb99466dd0dc91a92a6
Tayer <tayergustavo@hotmail.com>
2022-09-11 12:49:09 -0300

    cleaning

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,11 +4,9 @@ https://docs.nestjs.com/providers#services
 
 import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { createCipheriv, createDecipheriv, randomBytes, scrypt, scryptSync } from 'crypto';
 import { CreateUser } from 'src/user/dto/createUser.dto';
 import { User } from 'src/user/user.entity';
 import { UserService } from 'src/user/user.service';
-import { promisify } from 'util';
 import {genSaltSync, hashSync, compareSync} from 'bcrypt'
 
 @Injectable()
@@ -18,7 +16,6 @@ export class AuthService {
 
     async validate(name: string, password: string): Promise<User> {
         const user = await this.userService.findByName(name)
-        console.log('senhaaa', password)
         if (!user || !this.validatePassword(user, password)) {
             throw new UnauthorizedException();
         }
@@ -35,21 +32,11 @@ export class AuthService {
         return this.userService.create(user)
     }
 
-    private getSecrets(): any {
-        return {
-            iv: randomBytes(16),
-            key: '12345678901234567890123456789012' // this should be hidden 
-        }
-    }
-
     private validatePassword(user: User, passwordInformed: string): boolean {
-        console.log(user, passwordInformed)
         return compareSync(passwordInformed, user.password)
     }
 
     async login(loginUser: any) {
-        // const user = await this.validate(loginUser.username, loginUser.password)
-        console.log(loginUser)
         const payload = loginUser
         return {
             access_token: this.jwtService.sign(payload),
